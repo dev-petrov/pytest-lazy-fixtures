6e34c60f1e527efc5cf4c9cb9a3fe775a1112373
Stanislav Khlud <43534015+TheSuperiorStanislav@users.noreply.github.com>
2024-03-16 21:04:10 +0700

    Add shortcut for lazy fixtures (#12)

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pytest_lazy_fixtures import lf, lfc
 from pytest_lazy_fixtures.lazy_fixture import LazyFixtureWrapper
 from pytest_lazy_fixtures.lazy_fixture_callable import LazyFixtureCallableWrapper
 from tests.entity import DataNamedTuple, Entity
@@ -14,7 +13,16 @@ def test_lazy_fixture_callable_repr():
     assert str(LazyFixtureCallableWrapper("fixture")) == "fixture"
 
 
-@pytest.mark.parametrize(("first", "second", "other"), [(lf("one"), lf("two"), 4)])
+@pytest.mark.parametrize(
+    ("first", "second", "other"),
+    [
+        (
+            pytest.lazy_fixtures("one"),
+            pytest.lazy_fixtures("two"),
+            4,
+        ),
+    ],
+)
 def test_lazy_fixture(first, second, other):
     assert first == 1
     assert second == 2
@@ -24,12 +32,34 @@ def test_lazy_fixture(first, second, other):
 @pytest.mark.parametrize(
     ("actual", "expected"),
     [
-        ({"a": [lf("one"), lf("two")]}, {"a": [1, 2]}),
-        ({"a": {lf("one"), lf("two")}}, {"a": {1, 2}}),
-        ({lf("three"), lf("two"), lf("three")}, {3, 2}),
-        ([lf("three"), lf("two")], [3, 2]),
         (
-            {"first_level": {"second_level": (lf("one"), lf("one"), lf("two")), "not_lazy_fixture": 1}},
+            {"a": [pytest.lazy_fixtures("one"), pytest.lazy_fixtures("two")]},
+            {"a": [1, 2]},
+        ),
+        (
+            {"a": {pytest.lazy_fixtures("one"), pytest.lazy_fixtures("two")}},
+            {"a": {1, 2}},
+        ),
+        (
+            {
+                pytest.lazy_fixtures("three"),
+                pytest.lazy_fixtures("two"),
+                pytest.lazy_fixtures("three"),
+            },
+            {3, 2},
+        ),
+        ([pytest.lazy_fixtures("three"), pytest.lazy_fixtures("two")], [3, 2]),
+        (
+            {
+                "first_level": {
+                    "second_level": (
+                        pytest.lazy_fixtures("one"),
+                        pytest.lazy_fixtures("one"),
+                        pytest.lazy_fixtures("two"),
+                    ),
+                    "not_lazy_fixture": 1,
+                },
+            },
             {"first_level": {"second_level": (1, 1, 2), "not_lazy_fixture": 1}},
         ),
     ],
@@ -38,12 +68,20 @@ def test_lazy_fixture_data_types(actual, expected):
     assert actual == expected
 
 
-@pytest.mark.parametrize("item", [lf("four")])
+@pytest.mark.parametrize("item", [pytest.lazy_fixtures("four")])
 def test_lazy_fixture_with_fixtures(item):
     assert item == 4
 
 
-@pytest.mark.parametrize("only_entity,entity_value", [(lf("entity"), lf("entity.value"))])
+@pytest.mark.parametrize(
+    "only_entity,entity_value",
+    [
+        (
+            pytest.lazy_fixtures("entity"),
+            pytest.lazy_fixtures("entity.value"),
+        ),
+    ],
+)
 def test_lazy_fixture_with_attrs(only_entity, entity_value):
     assert isinstance(only_entity, Entity)
     assert entity_value == 1
@@ -52,11 +90,11 @@ def test_lazy_fixture_with_attrs(only_entity, entity_value):
 @pytest.mark.parametrize(
     "message",
     [
-        lfc(
+        pytest.lazy_fixtures_callable(
             "There is two lazy fixture args, {} and {}! And one kwarg {field}! And also simple value {simple}".format,
-            lf("one"),
-            lf("two"),
-            field=lf("three"),
+            pytest.lazy_fixtures("one"),
+            pytest.lazy_fixtures("two"),
+            field=pytest.lazy_fixtures("three"),
             simple="value",
         ),
     ],
@@ -65,12 +103,28 @@ def test_lazy_fixture_callable_with_func(message):
     assert message == "There is two lazy fixture args, 1 and 2! And one kwarg 3! And also simple value value"
 
 
-@pytest.mark.parametrize("formatted", [lfc("entity_format", lf("entity"))])
+@pytest.mark.parametrize(
+    "formatted",
+    [
+        pytest.lazy_fixtures_callable(
+            "entity_format",
+            pytest.lazy_fixtures("entity"),
+        ),
+    ],
+)
 def test_lazy_fixture_callable_with_lf(formatted, entity):
     assert formatted == {"value": entity.value}
 
 
-@pytest.mark.parametrize("result", [lfc("entity.sum", lf("two"))])
+@pytest.mark.parametrize(
+    "result",
+    [
+        pytest.lazy_fixtures_callable(
+            "entity.sum",
+            pytest.lazy_fixtures("two"),
+        ),
+    ],
+)
 def test_lazy_fixture_callable_with_attr_lf(result):
     assert result == 3
 
